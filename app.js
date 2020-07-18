97cff6953d2c5cb19cf70dbad47c5a8bd141d72c
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-07-17 22:15:15 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+  useNewUrlParser: true,
+});
+
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({
+  name: 'Get After It',
+});
+
+const item2 = new Item({
+  name: 'Game Time',
+});
+
+const item3 = new Item({
+  name: 'Play to Win The Game',
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, (err) => {
+  err ? console.log(err) : console.log('Successfully added items to DB');
+});
+
+app.get('/', function (req, res) {
+  res.render('list', { listTitle: 'Today', newListItems: items });
+});
+
+app.post('/', function (req, res) {
+  const item = req.body.newItem;
+
+  if (req.body.list === 'Work') {
+    workItems.push(item);
+    res.redirect('/work');
+  } else {
+    items.push(item);
+    res.redirect('/');
+  }
+});
+
+app.get('/work', function (req, res) {
+  res.render('list', { listTitle: 'Work List', newListItems: workItems });
+});
+
+app.get('/about', function (req, res) {
+  res.render('about');
+});
+
+app.listen(3000, function () {
+  console.log('Server started on port 3000');
+});
