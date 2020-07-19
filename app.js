10112063399fb542c174a76d2d168df9183824fc
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-07-19 12:44:34 -0400

    setting up mongo atlas

--- app.js
+++ app.js
@@ -10,10 +10,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27017/todolistDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  'mongodb+srv://admin-dcasely:Test123@cluster0.ogyxo.mongodb.net/todolistDB',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const itemsSchema = {
   name: String,
